a88d23d2007653bb5d58c26834406323a263f6ca
sjoonb <sungjun9808@gmail.com>
2021-12-22 03:47:24 +0900

    chore: init

---
+++ MessageSender/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MessageSender
+//
+//  Created by 백성준 on 2021/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+
+        
+    }
+
+
+}
+
