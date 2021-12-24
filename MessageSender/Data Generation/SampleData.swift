2f719c175d96418e28fdabcb69686cb84283ebe2
sjoonb <sungjun9808@gmail.com>
2021-12-24 19:29:24 +0900

    fix: make sender's image can be nil

--- MessageSender/Data Generation/SampleData.swift
+++ MessageSender/Data Generation/SampleData.swift
@@ -15,7 +15,7 @@ final internal class SampleData {
     static let shared = SampleData()
     private init() {}
     
-    let sender = MockUser(senderId: "000000", displayName: "User")
+    let sender = MockUser(senderId: "000000", displayName: "Steve Jobs")
     
     var messageCount: Int = 0
 
@@ -48,8 +48,8 @@ final internal class SampleData {
         let lastName = sender.displayName.components(separatedBy: " ").first
         let initials = "\(firstName?.first ?? "A")\(lastName?.first ?? "A")"
      
-       
-        return Avatar(image:  #imageLiteral(resourceName: "Steve-Jobs"), initials: initials)
+        let image = UIImage(named: "Steve-Jobs")
+        return Avatar(image: image, initials: initials)
     }
     
 }
